792b5f6f8496c86e3fa55106921f94f70580a6cd
yanggujun <alanier@sina.cn>
2017-07-20 10:10:18 -0400

    remove redundant code

--- subscribes.py
+++ subscribes.py
@@ -5,5 +5,4 @@ class SubscribesController(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         subs = [{'name': 'programming'}, {'name': 'soccer'}, {'name': 'csharp'}]
-        jsn = json.dumps(subs)
         resp.body = json.dumps(subs)
